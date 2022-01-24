b68fa570ebc44a45c94255fb0d5ab6b3de1890f7
Neal Donnan <neal.donnan@entelect.co.za>
2022-01-24 14:57:39 +0200

    Added sample test class

---
+++ src/test/java/za/co/entelect/tddretreat/GameOfLifeTest.java
@@ -0,0 +1,13 @@
+package za.co.entelect.tddretreat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameOfLifeTest {
+
+  @Test
+  public void test() {
+    assertEquals(1, 2);
+  }
+}
